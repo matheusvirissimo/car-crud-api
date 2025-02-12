276d748715c7ab6fa7ed8cf793b3cc25ab802ac1
matheusvirissimo <matheus.virissimo@unesp.br>
2025-02-12 04:54:22 -0300

    update main and AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// arquivo que cria a aplicação Nest
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule); // padrão de projeto Factory
+  await app.listen(process.env.PORT ?? 3000); // está na porta 3000
 }
 bootstrap();
